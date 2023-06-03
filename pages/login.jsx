ae461e12ca6e14e45757e41dcae53fcdf168cc4f
sridhram <srdhrramkumar@gmail.com>
2023-06-03 23:20:00 +0530

    layout added to login page

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,18 +1,19 @@
-import { useSession, signIn, signOut } from "next-auth/react"
+import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
 export default function Component() {
   const { data: session } = useSession()
   if (session) {
     return (
-      <>
+      <Layout>
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
-      </>
+      </Layout>
     )
   }
   return (
-    <>
+    <Layout>
       Not signed in <br />
       <button onClick={() => signIn('google')}>Sign in</button>
-    </>
+    </Layout>
   )
 }
